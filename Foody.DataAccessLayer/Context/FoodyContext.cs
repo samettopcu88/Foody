b7c2d568d6b781f0df4b50623ca53183dd70eabd
samettopcu88 <samet.topcu.88@icloud.com>
2025-01-14 01:05:59 +0300

    PresentationLayer düzenlendi.

---
+++ Foody.DataAccessLayer/Context/FoodyContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Context
+{
+    public class FoodyContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("SERVER=SZYMEK; initial catalog=FoodyDb; integrated security=true");
+        }
+    }
+}
