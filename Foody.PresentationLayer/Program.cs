e79f74b81d9f590057a4fc0cc2211f3a17671ebc
samettopcu88 <samet.topcu.88@icloud.com>
2025-01-24 20:35:10 +0300

    ProductList yapıldı ve Product-Category ilişkisi tanımlandı.

--- Foody.PresentationLayer/Program.cs
+++ Foody.PresentationLayer/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<FoodyContext>();
 
 builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<IProductDal, EfProductDal>();
+builder.Services.AddScoped<IProductService, ProductManager>();
 
 builder.Services.AddControllersWithViews();
 
