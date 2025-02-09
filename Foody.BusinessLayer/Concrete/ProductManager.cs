06251becbf251006393ccacbd66e15a9b986da09
samettopcu88 <samet.topcu.88@icloud.com>
2025-02-09 23:32:10 +0300

    Product data girişleri ve arayüzü.

--- Foody.BusinessLayer/Concrete/ProductManager.cs
+++ Foody.BusinessLayer/Concrete/ProductManager.cs
@@ -32,6 +32,12 @@ namespace Foody.BusinessLayer.Concrete
         {
             _productDal.Insert(entity);
         }
+
+        public List<Product> TProductListWithCategory()
+        {
+            return _productDal.ProductListWithCategory();
+        }
+
         public void TUpdate(Product entity)
         {
             _productDal.Update(entity);
--- Foody.PresentationLayer/Controllers/ProductsController.cs
+++ Foody.PresentationLayer/Controllers/ProductsController.cs
@@ -20,7 +20,8 @@ namespace Foody.PresentationLayer.Controllers
 
         public IActionResult ProductListWithCategory()
         {
-            return View();
+            var values=_productService.TProductListWithCategory();
+            return View(values);
         }
     }
 }
